424f030a7c3cad11a3aab43355dacecfb68e03a8
Thiago Brandão <thiagothgb@gmail.com>
2020-06-06 15:20:18 -0400

    Finalização do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,60 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balanceArray: Balance[] = this.transactions.map(item => {
+      if (item.type === 'income') {
+        return {
+          income: item.value,
+          outcome: 0,
+          total: 0,
+        };
+      }
+
+      if (item.type === 'outcome') {
+        return {
+          income: 0,
+          outcome: item.value,
+          total: 0,
+        };
+      }
+
+      return {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      };
+    });
+
+    const balance = balanceArray.reduce(
+      (prev, curr) => {
+        const income = prev.income + curr.income;
+        const outcome = prev.outcome + curr.outcome;
+        return {
+          income,
+          outcome,
+          total: income - outcome,
+        };
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
